fa9e3b7a3d7267f4441cbbeb2d56fa6dc0456a55
Angstremoff <Angstremoff@ya.ru>
2025-02-19 21:50:10 +0100

    fix: add headers and redirects for static files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
   base: '/',
   plugins: [react()]
 })
