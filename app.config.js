799d168d5f44fbfb5944ff6e8e4149ff27bd9fde
angstremoff <angstremoff@ya.ru>
2026-03-17 21:16:29 +0100

    11

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default ({ config }) => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.anonymous.DomGoMobile',
-      buildNumber: '13'
+      buildNumber: '14'
     },
     android: {
       allowBackup: false,
@@ -37,7 +37,7 @@ export default ({ config }) => {
         backgroundColor: '#ffffff'
       },
       package: 'domgo.rs',
-      versionCode: 13,
+      versionCode: 14,
       config: {
         googleMaps: {
           apiKey: GOOGLE_MAPS_API_KEY
