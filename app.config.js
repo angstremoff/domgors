473b749e381aea12f43fc142d775efa7e8864032
angstremoff <angstremoff@ya.ru>
2026-03-20 19:26:41 +0100

    release: bump app version to 1.0.11

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default ({ config }) => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.anonymous.DomGoMobile',
-      buildNumber: '14'
+      buildNumber: '16'
     },
     android: {
       allowBackup: false,
@@ -37,7 +37,7 @@ export default ({ config }) => {
         backgroundColor: '#ffffff'
       },
       package: 'domgo.rs',
-      versionCode: 14,
+      versionCode: 16,
       config: {
         googleMaps: {
           apiKey: GOOGLE_MAPS_API_KEY
